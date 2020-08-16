1d589c2994d1ee4035d408aab1c6140c9be7f68a
WhiteSnowPinkPinkWhatItLike <l1356355470@163.com>
2020-08-16 19:30:08 +0800

    :oden: Fork 代码并更新 KaTex 版本和 auto-render.js
    我不行你能看到这句话

---
+++ Plugin.php
@@ -0,0 +1,81 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+/**
+ * MarkdownParse Katex for Typecho (Modified by memset0 & Snowflake_Pink)
+ * 
+ * @package MardownKatex
+ * @author zyuzhi & memset0
+ * @version 1.0.1.233
+ * @link https://blog.zyuzhi.me
+ */
+class MarkdownKatex_Plugin implements Typecho_Plugin_Interface
+{
+    public static $count = 0;
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+        Typecho_Plugin::factory('Widget_Abstract_Contents')->markdown = array('MarkdownKatex_Plugin','parseDown');
+        Typecho_Plugin::factory('Widget_Archive')->footer = array('MarkdownKatex_Plugin','footer');
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){}
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+    }
+    
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+    public static function parseDown($text)
+    {
+        require_once dirname(__FILE__) . '/ParsedownKatex.php';
+        $content = ParsedownKatex::instance()->setFencedCodeBlockClassPrefix('')->text($text);
+        return $content;
+    }
+
+    public static function footer($content) {
+        ?>
+<link href="//cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" rel="stylesheet">
+<script src="//cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js"></script>
+<script>
+var katex_config = {
+	delimiters: 
+	[
+		{left: "$$", right: "$$", display: true},
+  		{left: "$", right: "$", display: false}
+	]
+};
+</script>
+<script defer src="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/contrib/auto-render.min.js" onload="renderMathInElement(document.body,katex_config)"></script>
+<?php
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# typecho-plugins-katex-with-pjax
- 支持 Katex 的 Pjax 重载
+# Typecho Plugins KaTex with Pjax / Ajax
+这个插件基于 [memset0](https://github.com/memset0) 修改 [zyuzhi](https://github.com/zyuzhi) 的项目 [typecho-plugin-katex](https://github.com/memset0/typecho-plugin-katex) 二次修改而来的，如果有时间，会保持长期更新。
+
+
+
+细微修改如下：
+
+* 引入了官方的 `auto-render.min.js` 
+* 支持 `Pjax` / `Ajax` 回调函数
+
+```js
+renderMathInElement(document.body,katex_config)
+```
+
+## 使用
+
+导入 `Typecho` 目录 `/usr/plugins/` ，**把插件文件夹改名为 `MarkdownKatex` 后**，在控制台启用插件。
+
+若需要使用重载函数，请把重载函数代码放在主题专有的设置里面（需要 `Typecho` 主题支持）
+
